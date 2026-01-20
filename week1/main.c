2269c896fb6c460b7c20ba4d63b9c2810f7eaa92
Sauleh Eetemadi <sauleh@gmail.com>
2026-01-20 12:53:27 +0400

    u

---
+++ week1/main.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()                     // 5ffe8 16+4 - 5ffe8c
+{
+    int a;
+    a = 5;
+    int x, y=0xfff, z;
+    char b='B', c='A', d='C';
+    int zz = 0xffffff;
+    printf("hello world %d %d", z, x);
+    return 10;    
+}
